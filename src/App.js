3d8f532b7627b902106ed45d01f86116acb6cc99
Kathy Cuevas <meetnerdykat@gmail.com>
2019-05-05 20:56:45 -0400

    created new components, routes, added bootstrap, node-sass, imported assets. Still need to fix dropdowm and some more images but it is looking nice so far.

--- src/App.js
+++ src/App.js
@@ -7,15 +7,59 @@ import PrivateRoute from './components/PrivateRoute';
 import BublPage from './components/BublPage';
 import Bubl from './components/Bubl';
 import Home from './components/Home';
+import About from './components/About';
+import Team from './components/Team';
 import { NavBar } from './StyledComponents';
 import './App.scss';
 import './scss/styles.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min.js';
+import 'popper/dist/popper.min.js';
+import brand from './assets/PNG/INDIVIDUAL/brand.png';
 
 function App() {
   return (
     <div className="App">
       <div>
-        <NavBar>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
+          <div className="container">
+            <NavLink className="navLogo" to="/home">
+              <img height="90" src={brand} alt="Brand" />
+            </NavLink>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon" />
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav ml-auto">
+                <NavLink to="/about" activeClassName="activeNav">
+                  About Bubl
+                </NavLink>
+                <NavLink to="/team" activeClassName="activeNav">
+                  The Team
+                </NavLink>
+                <NavLink to="/login" activeClassName="activeNav">
+                  Login
+                </NavLink>
+                <NavLink to="/register" activeClassName="activeNav">
+                  Register
+                </NavLink>
+                <NavLink to="/schools" activeClassName="activeNav">
+                  Schools
+                </NavLink>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+        {/* <NavBar>
           <NavLink to="/login" activeClassName="activeNav">
             Login
           </NavLink>
@@ -25,7 +69,10 @@ function App() {
           <NavLink to="/schools" activeClassName="activeNav">
             Schools
           </NavLink>
-        </NavBar>
+        </NavBar> */}
+        <Route path="/home" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/team" component={Team} />
         <Route path="/login" component={Login} />
         <PrivateRoute exact path="/schools" component={SchoolsList} />
         {/* <Route exact path="/schools" component={SchoolsList} /> */}
@@ -33,7 +80,7 @@ function App() {
         <Route path="/schools/:id" component={BublPage} />
         <Route exact path="/bubl/:id" component={Bubl} />
 
-        <Home />
+        {/* <Home /> */}
       </div>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
+import kids from '../assets/PNG/INDIVIDUAL/kids.png';
+import bubl from '../assets/PNG/INDIVIDUAL/bubl.png';
 
 class Home extends Component {
   render() {
     return (
       <div>
-
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 content-titles">
+              <h1 className="content-title title-one">find friends</h1>
+              <h1 className="content-title title-two">have fun</h1>
+              <h1 className="content-title title-three">grow your</h1>
+              <img height="90" src={bubl} alt="bubl logo" />
+              <div className="row cta-buttons">
+                <NavLink to="/register" className="btn-block signupBtn">
+                  SignUp
+                </NavLink>
+              </div>
+              <div className="row cta-buttons">
+                <NavLink to="/login" className="btn-block loginBtn">
+                  Login
+                </NavLink>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <img
+                className="kidsImage"
+                height="500"
+                src={kids}
+                alt="illustration"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
